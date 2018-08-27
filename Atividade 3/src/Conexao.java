751d67e377beb4be04b5f22e24323e2e0514a8a7
Felipe Malheiros Barros <felipemalheirosb@gmail.com>
2018-08-26 23:32:36 -0300

    Add files via upload

---
+++ Atividade 3/src/Conexao.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/** 
+ * Atividade: Aplique o padrão Singleton na classe abaixo. 
+ *
+ **/
+public class Conexao {
+	
+	private Conexao () {   }
+    private static Connection con = null;
+    private static Conexao conexaoInstance;
+    
+    public static Conexao getInstance() throws ClassNotFoundException {
+		if(conexaoInstance == null) {
+			conexaoInstance = new Conexao();
+			System.out.println("Conectando ao banco...");
+	        try {
+	            Class.forName("com.mysql.jdbc.Driver");
+	            con =  DriverManager.getConnection("jdbc:mysql://127.0.0.1/banco","usuario","senha");
+	            System.out.println("Conectado.");
+	        } catch(SQLException e) {
+	              System.out.println(e);
+	          }
+		}
+		return conexaoInstance;
+	}
+
+}
---
+++ Atividade 3/src/ConexaoMain.java
@@ -0,0 +1,12 @@
+
+public class ConexaoMain {
+	
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		Conexao conexao1 = Conexao.getInstance();
+		Conexao conexao2 = Conexao.getInstance();
+		
+		
+	}
+
+}
